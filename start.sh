024684dee73708177920ce1aac1a93060361b9e5
Aqilah <qilasolo20@gmail.com>
2026-08-12 14:26:05 +0700

    upd

--- start.sh
+++ start.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
- 
-# Jalankan migration otomatis saat startup (opsional, hapus kalau tidak mau)
+
+# Jalankan migration otomatis saat startup
+php artisan migrate --force
+
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
- 
+
 # Jalankan PHP-FPM di background
 php-fpm -D
- 
+
 # Jalankan Nginx di foreground (biar container tetap hidup)
 nginx -g "daemon off;"
- 
